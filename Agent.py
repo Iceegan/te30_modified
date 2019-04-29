1b3490bbc4bd91959c70299fc9a3b6d82a138050
Iceegan <keegancampanelli@gmail.com>
2019-04-29 13:22:23 -0500

    Wrote agent for interface with Transax

--- Agent.py
+++ Agent.py
@@ -3,6 +3,8 @@ import json
 import sys
 import tesp_support.fncs as fncs
 import tesp_support.simple_auction as simple_auction
+import zmq
+
 if sys.platform != 'win32':
 	import resource
 
@@ -11,25 +13,81 @@ time_granted = 0
 
 fncs.initialize()
 
+import time
+import zmq
+
+context = zmq.Context(1)
+server = context.socket(zmq.REP)
+server.bind("tcp://*:5555")
+
+
+batt_charges = {}
+solar_power = {}
+
 while time_granted < time_stop:
-	time_granted = fncs.time_request(time_stop)
 	events = fncs.get_events()
-	#print(time_granted, events, flush=True)
 	for topic in events:
 		value = fncs.get_value(topic)
-		#can improve this by creating a set of all the relevant topics
-		#Check if topic is in the set
-		if(topic == 'step'):
-			print(time_granted, topic, value, flush=True)
-			print(len(events))
-		#if ('batt_charge' in topic):
-			#print('Charge topic')
-		#	batt_num = topic[0:4]
-		#	if simple_auction.parse_fncs_number(value) > 0.95:
-				#print('inside charge topic')
-				#print(batt_num, '_batt_P_Out')
-		#		fncs.publish(batt_num + '_batt_P_Out', 1000)
+		#Collect Most recent charge data
+		if('batt_charge' in topic):
+			batt_charges[topic] = value
+		if('solar_power' in topic):
+			solar_power[topic] = value
+	if('step' in events):
+		request = ''
+		while(request != 'step'):
+			msg = server.recv_pyobj()
+			request = msg['request']
+			if (request == 'postTrade'):
+				print(request, time_granted, flush=True)
+				req_batt_num = msg['ID']
+				first = req_batt_num[0:1]
+				second = req_batt_num[1:3]
+				if (second[0] == '0'):
+					second = second[1]
+				m_batt_num = 'b' + first + 'm' + second
+				keyname = m_batt_num + '_solar_power'
+				if(keyname in solar_power):
+					batt_out = msg['power'] + float(solar_power[keyname].split(' ')[0])
+					print('msg[\'power\']: ', str(msg['power']),flush=True)
+					print('solar power:', str(float(solar_power[keyname].split(' ')[0])),flush=True)
+					print('batt_out:', str(batt_out),flush=True)
+				else:
+					batt_out = 0
+				fncs.publish(m_batt_num + '_batt_P_Out', str(batt_out))
+				print('fncs published:', m_batt_num + '_batt_P_Out', str(batt_out))
+				server.send_pyobj('Trade Posted')
+			if (request == 'charge'):
+				print(request, time_granted, flush=True)
+				req_batt_num = msg['ID']
+				first = req_batt_num[0:1]
+				second = req_batt_num[1:3]
+				if (second[0] == '0'):
+					second = second[1]
+				m_batt_num = 'b' + first + 'm' + second
+				keyname = m_batt_num + '_batt_charge'
+				if(keyname in batt_charges):
+					charge = batt_charges[keyname]
+				else:
+					charge = '+0 pu'
+				server.send_pyobj(charge)
+		print(request, time_granted, flush=True)
+		time_granted = fncs.time_request(time_stop)
+		server.send_pyobj('Step Granted')
+		"""if(time_granted >= time_stop):
+			server.send_pyobj('end')
+		else:"""
+	else:
+		time_granted = fncs.time_request(time_stop)
+		print("Simulation Continue, new time_granted:", str(time_granted))
 
+
+
+print('End of Simulation', flush=True)
+
+server.close()
+context.term()
+print('zmq Closed', flush=True)
 fncs.finalize()
 
 if sys.platform != 'win32':
