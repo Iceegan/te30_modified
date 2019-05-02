e7a337be932547494eb23c6c1e4a236effd40ce9
Iceegan <keegancampanelli@gmail.com>
2019-05-01 20:29:18 -0500

    Added csv metrics and fixed inverted solar power bug

--- Agent.py
+++ Agent.py
@@ -3,81 +3,86 @@ import json
 import sys
 import zmq
 import fncs
+import time
 
 if sys.platform != 'win32':
 	import resource
+
+
+RIAPS = True 
 
 time_stop = int(sys.argv[1])
 time_granted = 0
 
 fncs.initialize()
 
-import time
-import zmq
 
-context = zmq.Context(1)
-server = context.socket(zmq.REP)
-server.bind("tcp://*:5555")
+if(RIAPS):
+	context = zmq.Context(1)
+	server = context.socket(zmq.REP)
+	server.bind("tcp://*:5555")
 
 
-batt_charges = {}
-solar_power = {}
+	batt_charges = {}
+	solar_power = {}
 
-while time_granted < time_stop:
-	events = fncs.get_events()
-	print(events,flush=True)
-	"""for topic in events:
-		value = fncs.get_value(topic)
-		#Collect Most recent charge data
-		if('batt_charge' in topic):
-			batt_charges[topic] = value
-		if('solar_power' in topic):
-			solar_power[topic] = value
-	if('step' in events):
-		request = ''
-		while(request != 'step'):
-			msg = server.recv_pyobj()
-			request = msg['request']
-			if (request == 'postTrade'):
-				print(request, time_granted, flush=True)
-				req_batt_num = msg['ID']
-				first = req_batt_num[0:1]
-				second = req_batt_num[1:3]
-				if (second[0] == '0'):
-					second = second[1]
-				m_batt_num = 'b' + first + 'm' + second
-				keyname = m_batt_num + '_solar_power'
-				if(keyname in solar_power):
-					batt_out = msg['power'] + float(solar_power[keyname].split(' ')[0])
-					print('msg[\'power\']: ', str(msg['power']),flush=True)
-					print('solar power:', str(float(solar_power[keyname].split(' ')[0])),flush=True)
-					print('batt_out:', str(batt_out),flush=True)
-				else:
-					batt_out = 0
-				fncs.publish(m_batt_num + '_batt_P_Out', str(batt_out))
-				print('fncs published:', m_batt_num + '_batt_P_Out', str(batt_out))
-				server.send_pyobj('Trade Posted')
-			if (request == 'charge'):
-				print(request, time_granted, flush=True)
-				req_batt_num = msg['ID']
-				first = req_batt_num[0:1]
-				second = req_batt_num[1:3]
-				if (second[0] == '0'):
-					second = second[1]
-				m_batt_num = 'b' + first + 'm' + second
-				keyname = m_batt_num + '_batt_charge'
-				if(keyname in batt_charges):
-					charge = batt_charges[keyname]
-				else:
-					charge = '+0 pu'
-				server.send_pyobj(charge)
-		print(request, time_granted, flush=True)
+	while time_granted < time_stop:
+		events = fncs.get_events()
+		for topic in events:
+			value = fncs.get_value(topic)
+			#Collect Most recent charge data
+			if('batt_charge' in topic):
+				batt_charges[topic] = value
+			if('solar_power' in topic):
+				solar_power[topic] = value
+		if('step' in events):
+			request = ''
+			while(request != 'step'):
+				msg = server.recv_pyobj()
+				request = msg['request']
+				if (request == 'postTrade'):
+					print(request, time_granted, flush=True)
+					req_batt_num = msg['ID']
+					first = req_batt_num[0:1]
+					second = req_batt_num[1:3]
+					if (second[0] == '0'):
+						second = second[1]
+					m_batt_num = 'b' + first + 'm' + second
+					keyname = m_batt_num + '_solar_power'
+					if(keyname in solar_power):
+						#Solar power is now positive in GridLabD
+						batt_out = msg['power'] - float(solar_power[keyname].split(' ')[0])
+						print('msg[\'power\']: ', str(msg['power']),flush=True)
+						print('solar power:', str(float(solar_power[keyname].split(' ')[0])),flush=True)
+						print('batt_out:', str(batt_out),flush=True)
+					else:
+						batt_out = 0
+					fncs.publish(m_batt_num + '_batt_P_Out', str(batt_out))
+					print('fncs published:', m_batt_num + '_batt_P_Out', str(batt_out))
+					server.send_pyobj('Trade Posted')
+				if (request == 'charge'):
+					print(request, time_granted, flush=True)
+					req_batt_num = msg['ID']
+					first = req_batt_num[0:1]
+					second = req_batt_num[1:3]
+					if (second[0] == '0'):
+						second = second[1]
+					m_batt_num = 'b' + first + 'm' + second
+					keyname = m_batt_num + '_batt_charge'
+					if(keyname in batt_charges):
+						charge = batt_charges[keyname]
+					else:
+						charge = '+0 pu'
+					server.send_pyobj(charge)
+			print(request, time_granted, flush=True)
+			time_granted = fncs.time_request(time_stop)
+			server.send_pyobj('Step Granted')
+		else:
+			time_granted = fncs.time_request(time_stop)
+			print("Simulation Continue, new time_granted:", str(time_granted))
+else:
+	while time_granted < time_stop:
 		time_granted = fncs.time_request(time_stop)
-		server.send_pyobj('Step Granted')
-	else:
-		time_granted = fncs.time_request(time_stop)
-		print("Simulation Continue, new time_granted:", str(time_granted))"""
-	time_granted=fncs.time_request(time_stop)
 
 
 
--- kill5570.sh
+++ kill5570.sh
@@ -1,2 +1,4 @@
-lsof -i tcp:5570 | awk 'NR!=1 {print $2}' | xargs kill
+lsof -i tcp:5570 | awk 'NR!=1 {print $2}' | xargs kill -9
+lsof -i tcp:5555 | awk 'NR!=1 {print $2}' | xargs kill -9
+pkill python3
 
